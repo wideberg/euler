48b74d761255a528e643d317dd5a0cd04cb33c37
Magnus Wideberg <magnus.wideberg@gmail.com>
2010-06-07 17:00:12 +0200

    Added non-working version for problem 112

---
+++ e112.hs
@@ -0,0 +1,31 @@
+isDecreasing :: Integer -> Bool
+isDecreasing x = isDecreasing' 0 x
+               
+isDecreasing' :: Integer -> Integer -> Bool
+isDecreasing' _ 0 = True
+isDecreasing' c x =
+    c <= last && isDecreasing' last first
+        where last = mod x 10
+              first = div x 10
+
+isIncreasing :: Integer -> Bool
+isIncreasing x = isIncreasing' 9 x
+
+isIncreasing' :: Integer -> Integer -> Bool
+isIncreasing' _ 0 = True
+isIncreasing' c x =
+    c >= last && isIncreasing' last first
+        where last = mod x 10
+              first = div x 10
+
+isBouncy :: Integer -> Bool
+isBouncy x = (not (isIncreasing x)) && (not (isDecreasing x))
+
+bouncy :: [Integer]
+bouncy = filter isBouncy [1..]
+
+bouncy_ratio :: [Float]
+bouncy_ratio = map (\(x,y) -> (fromInteger x) /(fromInteger y) ) (zip [1..] bouncy)
+
+
+e112 = last (takeWhile (\(x,y) -> x <= 0.99) (zip bouncy_ratio bouncy))
